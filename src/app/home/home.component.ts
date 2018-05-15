a0c0dea0ef76c460114b4d01302ea80ab00c5167
Damian Juzyk <damian.juzyk@gmail.com>
2018-05-15 10:55:35 +0200

    add service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, transition, animate, query, keyframes, stagger } from '@angular/animations';
-
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   animations: [
+
     trigger('goals', [
       transition('* => *', [
-        query(':enter', style({ opacity: 0}), {optional: true}),
+
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
         query(':enter', stagger('300ms', [
           animate('.6s ease-in', keyframes([
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
-            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
-          ]))])),
-
-
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+          ,
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-out', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
     ])
-  ])
+
   ]
 })
 
@@ -28,22 +37,27 @@ export class HomeComponent implements OnInit {
   itemCount: number;
   btnText: string = "Add an item";
   goalText: string = 'My first goal';
-  goals = ['My first goal'];
+  goals = [];
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = '',
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   removeItem(i) {
     this.goals.splice(i, 1);
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
 }
